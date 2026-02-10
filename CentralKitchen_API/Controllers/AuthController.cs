f6cda815dbc00edff0b21d2372d7c0820f0e8cf8
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-02-10 15:04:48 +0700

    create AuthController

---
+++ CentralKitchen_API/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using CentralKitchen_Services.DTOs;
+using CentralKitchen_Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentralKitchen_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILoginService _service;
+        private readonly IJwtService _jwtService;
+        public AuthController(ILoginService service, IJwtService jwtService)
+        {
+            _service = service;
+            _jwtService = jwtService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequestDTO login)
+        {
+            var account = _service.Login(login.Email, login.Password);
+
+            if (account == null)
+            {
+                return BadRequest(new { Error = "HB40001", Message = "Missing/invalid input" });
+            }
+            
+
+            var token = _jwtService.GenerateToken(account);
+
+            return Ok(new
+            {
+                Status = "Success",
+                Username = account.Username,
+                Email = account.Email,
+                RoleId = account.RoleId,
+               RoleName = account.Role.RoleName,
+                Token = token,
+                
+                
+
+            });
+        }
+    }
+}
--- CentralKitchen_API/Program.cs
+++ CentralKitchen_API/Program.cs
@@ -1,8 +1,48 @@
+﻿using CentralKitchen_Repositories.Models;
+using CentralKitchen_Repositories.Repository;
+using CentralKitchen_Services.IServices;
+using CentralKitchen_Services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext <CentralKitchenDBContext> (options =>
+   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Dependency Injection
+builder.Services.AddScoped<LoginRepo>();
+builder.Services.AddScoped<ILoginService, LoginService>();
+var configuration = builder.Configuration;
+// 1. Đăng ký IJwtService
+builder.Services.AddScoped<IJwtService, JwtService>();
+
+// 2. Cấu hình JWT Bearer Authentication
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = configuration["JwtSettings:Issuer"],
+        ValidAudience = configuration["JwtSettings:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]))
+    };
+});
+
+// Thêm Authorization Middleware
+builder.Services.AddAuthorization();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,6 +58,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
