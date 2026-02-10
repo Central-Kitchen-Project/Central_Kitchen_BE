dd3893802f151661e8b9840a30caedd468525159
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-02-10 15:54:35 +0700

    create RegisterUser & UserExist, change name function GetAccount

--- CentralKitchen_Repositories/Repositories/LoginRepo.cs
+++ CentralKitchen_Repositories/Repositories/LoginRepo.cs
@@ -17,14 +17,21 @@ namespace CentralKitchen_Repositories.Repository
             _context = context;
         }
 
-        public User? GetAccount(string email, string password)
+        public User? GetAccountByEmail(string email)
         {
-           
             return _context.Users
-                 .Include(u => u.Role)
-                .FirstOrDefault(
-                   a => a.Email == email && a.PasswordHash == password
-            );
+                 .Include(u => u.Role) 
+                 .FirstOrDefault(a => a.Email == email);
+        }
+
+        public async Task<bool> RegisterUser(User user)
+        {
+            _context.Users.Add(user);
+            return await _context.SaveChangesAsync() > 0;
+        }
+        public async Task<bool> UserExists(string email)
+        {
+            return await _context.Users.AnyAsync(x => x.Email == email);
         }
     }
 }
