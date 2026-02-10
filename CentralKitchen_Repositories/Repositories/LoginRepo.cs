73be729cc0ae0598fac0f9eb938f1159937d29cf
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-02-10 15:04:02 +0700

    create login repo by email & password

---
+++ CentralKitchen_Repositories/Repositories/LoginRepo.cs
@@ -0,0 +1,30 @@
+﻿using CentralKitchen_Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralKitchen_Repositories.Repository
+{
+    public class LoginRepo 
+    {
+        private readonly CentralKitchenDBContext _context;
+
+        public LoginRepo(CentralKitchenDBContext context)
+        {
+            _context = context;
+        }
+
+        public User? GetAccount(string email, string password)
+        {
+           
+            return _context.Users
+                 .Include(u => u.Role)
+                .FirstOrDefault(
+                   a => a.Email == email && a.PasswordHash == password
+            );
+        }
+    }
+}
