e763c031decee4f7d1ea9d939d0c729d36abb0de
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-02-24 13:33:59 +0700

    Create InventoryDTOs, IInventoryService, InventoryService

---
+++ CentralKitchen_Services/IServices/IInventoryService.cs
@@ -0,0 +1,19 @@
+﻿using CentralKitchen_Repositories.Models;
+using CentralKitchen_Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralKitchen_Services.IServices
+{
+    public interface IInventoryService
+    {
+        Task<IEnumerable<InventoryDTO>> GetAllInventories();
+        Task<InventoryDTO> GetInventoryById(int id);
+        Task<bool> UpdateStock(int id, decimal newQuantity);
+    }
+
+
+}
