2d3ff69682d660ccc4806d41b452d768d92d23e9
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-03-03 16:24:38 +0700

    thêm tham số userId vào hàm GetAll và GetId

--- CentralKitchen_API/Controllers/InventoryController.cs
+++ CentralKitchen_API/Controllers/InventoryController.cs
@@ -16,26 +16,26 @@ namespace CentralKitchen_API.Controllers
 
         // 1. API Get All
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll(int userId)
         {
-            var result = await _service.GetAllInventories();
+            var result = await _service.GetAllInventories(userId);
             return Ok(result);
         }
 
         // 2. API Get By Id
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
+        public async Task<IActionResult> GetById(int id, int userId)
         {
-            var result = await _service.GetInventoryById(id);
+            var result = await _service.GetInventoryById(id, userId);
             if (result == null) return NotFound("Không tìm thấy bản ghi tồn kho.");
             return Ok(result);
         }
 
         // 3. API Update Inventory
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, [FromBody] decimal quantity)
+        public async Task<IActionResult> Update(int id, [FromBody] decimal quantity, int userId)
         {
-            var success = await _service.UpdateStock(id, quantity);
+            var success = await _service.UpdateStock(id, quantity, userId);
             if (!success) return BadRequest("Cập nhật thất bại. Vui lòng kiểm tra lại ID.");
 
             return Ok(new { message = "Cập nhật tồn kho thành công!" });
