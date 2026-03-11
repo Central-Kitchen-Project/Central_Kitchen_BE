cb1c5ada38b859445089b2a14ffbd9fff05c9ec1
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-03-11 15:08:59 +0700

    create class CreateFinishedProductDto & RecipeIngredientDto

---
+++ CentralKitchen_Services/DTOs/CreateFinishedProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralKitchen_Services.DTOs
+{
+    public class CreateFinishedProductDto
+    {
+        public int FinishedItemId { get; set; } // ID của món ăn/thành phẩm
+        public List<RecipeIngredientDto> Ingredients { get; set; } = new List<RecipeIngredientDto>();
+    }
+}
