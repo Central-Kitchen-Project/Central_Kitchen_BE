ccfd68b895d2942a105afb5fa46cfc86445809b8
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-03-11 16:57:44 +0700

    Create UpdateFinishedProductRecipeAsync in IRecipeService

---
+++ CentralKitchen_Services/IServices/IRecipeService.cs
@@ -0,0 +1,14 @@
+﻿using CentralKitchen_Services.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralKitchen_Services.IServices
+{
+    public interface IRecipeService
+    {
+        Task<bool> UpdateFinishedProductRecipeAsync(UpdateRecipeDto dto);
+    }
+}
