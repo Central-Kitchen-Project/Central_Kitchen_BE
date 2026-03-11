2caf7e790574385a887cee8653e7b0dbdcb2d744
Shmily3072k4 <quocdat2004.hs@gmail.com>
2026-03-11 15:59:22 +0700

    create ItemUpdateDto

---
+++ CentralKitchen_Services/DTOs/ItemUpdateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralKitchen_Services.DTOs
+{
+    public class ItemUpdateDto
+    {
+        public string ItemName { get; set; }
+        public string Unit { get; set; }
+        public string ItemType { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
